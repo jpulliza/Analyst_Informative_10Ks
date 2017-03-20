74065726342744d7ac53a3efd47ccec44af0bf5b
jonathan <jpulliza@gmail.com>
2017-03-20 09:50:15 -0400

    Added record_filings and made everyting into functions

--- Database_Operations.py
+++ Database_Operations.py
@@ -7,23 +7,35 @@ logging.basicConfig(filename='general_log.log', level=logging.INFO)
 
 db = dataset.connect('sqlite:////media/windows-share/SEC_Downloads/10K_Sentences.db')
 
-table = db['10K_Sentences']
 
-tickers, filing_years, file_locations = list_10k_files()
-
-for i in range(len(file_locations)):
+def record_sentences(ticker, filing_year, file_location):
+    table = db['10K_Sentences']
     db.begin()
     try:
-        sentences = parse_sentences(file_locations[i])
+        sentences = parse_sentences(file_location)
         for sentence in sentences:
-            table.insert(dict(ticker=tickers[i],
-                              filing_year=filing_years[i],
+            table.insert(dict(ticker=ticker,
+                              filing_year=filing_year,
+                              file_location=file_location,
                               sentence=sentence))
         db.commit()
-        logging.info("DB Loaded {0} - {1}".format(tickers[i], filing_years[i]))
+        logging.info("DB Loaded Sentences {0} - {1} - {2}".format(ticker, filing_year, file_location))
     except Exception:
-        logging.error("DB Error {0} - {1}".format(tickers[i], filing_years[i]))
+        logging.error("DB Error Sentences {0} - {1} - {2}".format(ticker, filing_year, file_location))
         db.rollback()
         pass
-    finally:
-        i += 1
+
+
+def record_filings(id, ticker, filing_year, file_location):
+    table = db['Filings']
+    db.begin()
+    try:
+        table.insert(dict(ticker=ticker,
+                          filing_year=filing_year,
+                          file_location=file_location))
+        db.commit()
+        logging.info("DB Loaded Filings {0} - {1} - {2} - {3}".format(id, ticker, filing_year, file_location))
+    except Exception:
+        logging.error("DB Error Filings {0} - {1} - {2} - {3}".format(id, ticker, filing_year, file_location))
+        db.rollback()
+        pass
